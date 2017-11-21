98a8daaa5357cf898ffcefaa2fd577d361a59bd6
Tiago Ornelas <titasornelas@gmail.com>
2017-11-21 15:29:59 +0100

    model created

--- models/user.js
+++ models/user.js
@@ -5,7 +5,19 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  firstName: String,
+  lastName: String,
+  phoneNumber: String, // STRING OR NUMBER???
+  email: String,
+  address: String,
+  socialMedia: {
+    facebook: String,
+    Instagram: String
+  },
+  contacts: {
+    user_id: Number
+  }
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
