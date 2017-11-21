a0d5acf04cfb439c93d6ea1b1aa89e27f5456a68
Jean Nothomb <jeannothomb@gmail.com>
2017-11-21 15:20:16 +0100

    adding sign up - login - private page

--- app.js
+++ app.js
@@ -5,41 +5,99 @@ const path = require("path");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
-
+const app = express();
 const index = require("./routes/index");
 const users = require("./routes/users");
-
-const app = express();
-
+const passportRouter = require("./routes/passportRouter");
+// mongoose configuration
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/automated-contact-manager");
+// require the user model
+const User = require("./models/user");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const flash = require("connect-flash");
+// enable sessions here
+app.use(flash());
+app.use(session({
+  secret: "automated-contact-manager-app",
+  resave: true,
+  saveUninitialized: true
+}));
+// initialize passport and session here
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+});
+passport.deserializeUser((id, cb) => {
+  User.findOne({
+    "_id": id
+  }, (err, user) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
+});
+passport.use(new LocalStrategy(
+  (username, password, next) => {
+    User.findOne({
+      username
+    }, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return next(null, false, {
+          message: "Incorrect username"
+        });
+      }
+      if (!bcrypt.compareSync(password, user.password)) {
+        return next(null, false, {
+          message: "Incorrect password"
+        });
+      }
+      return next(null, user);
+    });
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
 app.use(logger("dev"));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+// require in the routers
 app.use("/", index);
-app.use("/users", users);
-
+app.use("/", users);
+app.use("/", passportRouter);
+// passport code here
+app.use(
+  session({
+    secret: "automated-contact-manager-app",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
-
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
-
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
+"use strict";
+
 const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  if (req.isAuthenticated()) {
+    res.render("index", {
+      title: "your_ACM",
+      loggedIn: true
+    });
+  } else {
+    res.render("index", {
+      title: "your_ACM",
+      loggedIn: false
+    });
+  }
 });
-
 module.exports = router;
